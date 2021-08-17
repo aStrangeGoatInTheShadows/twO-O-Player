42b3f3acd25fb6d0e0055e0b21fc77f834fa5696
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-17 10:11:25 -0700

    moved Game_IO class into its own file

---
+++ game_io.rb
@@ -0,0 +1,17 @@
+#  This class will hand input and output methods for the user on behalf of the game loop
+class Game_IO 
+  # This class only cares about immediate input and output, it does not store any of its own information
+
+  def initialize ()
+    puts "game io initialized"
+  end
+
+  def ask_question (player_num)
+
+  end
+
+  def output_scores (p1, p2) 
+
+  end
+
+end
--- main.rb
+++ main.rb
@@ -13,23 +13,6 @@ end
 
 
 
-#  This class will hand input and output methods for the user on behalf of the game loop
-class Game_IO 
-  # This class only cares about immediate input and output, it does not store any of its own information
-
-  def initialize ()
-    puts "game io initialized"
-  end
-
-  def ask_question (player_num)
-
-  end
-
-  def output_scores (p1, p2) 
-
-  end
-
-end
 
 #  This class will run the main game loop, it will keep track of whoes turn it is, check for game ending evalutations, 
 class Game_Loop 
