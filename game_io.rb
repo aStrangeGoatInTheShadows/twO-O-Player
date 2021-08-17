c5f57e6edbaaa6a6d42198156449d61093c48948
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-17 10:51:43 -0700

    building out question input output

--- game_io.rb
+++ game_io.rb
@@ -7,11 +7,22 @@ class Game_IO
   end
 
   def ask_question (player_num)
+    question = Game_Math.new()
+    operand_plain_english = question.operand
 
+    puts "Player #{player_num}: What does #{question.a} #{operand_plain_english} #{question.b} equal?"
+    guess = gets.to_i
+
+    if question.is_correct(guess)
+      puts "Congradulations Player #{player_num}, you are smarter than a slug."
+    else
+      puts "Player #{player_num}, we gotta slap some toast on that head and make an idiot sandwhich."
+      puts "You have inched that much closer to your eventual demise"
+    end
   end
 
   def output_scores (p1, p2) 
-    puts "P1: #{p1.remaining_lives} / #{p1.STARTING_LIVES}"
+    puts "P1: #{p1.remaining_lives} / 3 VS P2: #{p2.remaining_lives} / 3"
   end
 
 end
